cad60768f70ddc0c9ea340651fd000efe32cea58
Vitor Seixas <vaseixascunha@gmail.com>
2026-09-23 03:13:13 +0000

    Pending changes exported from your codespace

--- teste2.c
+++ teste2.c
@@ -11,25 +11,25 @@ typedef struct data{
     }data;
 
 typedef struct aluno{
-    char cpf_aluno;
-    int matricula_aluno;
-    char nome_aluno [60];
+    char cpf_aluno[11];
+    char matricula_aluno[60];
+    char nome_aluno[60];
     data nascimento_aluno;
 
     }aluno;
 
 typedef struct professor{
-    char cpf_prof;
-    int matricula_prof;
-    char nome_prof [60];
+    char cpf_prof[11];
+    char matricula_prof[60];
+    char nome_prof[60];
     data nascimento_prof;
     
     }professor;
 
 typedef struct disciplina{
-    int codigo;
+    char codigo[60];
     int semestre;
-    char nome_dis [60];
+    char nome_dis[60];
     professor prof_da_disciplina[3];
 
     }disciplina;
@@ -39,7 +39,7 @@ void cadastra_aluno(aluno a0[], int i1){
     printf("nome do aluno:\n");
     fgets(a0[i1].nome_aluno, sizeof(a0[i1].nome_aluno), stdin);
     printf("matricula do aluno:\n");
-    scanf("%d", &a0[i1].matricula_aluno);
+    scanf("%s", &a0[i1].matricula_aluno);
     printf("cpf do aluno:\n");
     scanf("%s", &a0[i1].cpf_aluno);
 }
@@ -49,7 +49,7 @@ void cadastra_professor(professor p0[], int j1){
     printf("nome do professor:\n");
     fgets(p0[j1].nome_prof, sizeof(p0[j1].nome_prof), stdin);
     printf("matricula do professor:\n");
-    scanf("%d", &p0[j1].matricula_prof);
+    scanf("%s", &p0[j1].matricula_prof);
     printf("cpf do professor:\n");
     scanf("%s", &p0[j1].cpf_prof);
 }                        
@@ -59,51 +59,87 @@ void cadastra_disciplina(disciplina d0[], int k1){
     printf("nome da disciplina:\n");
     fgets(d0[k1].nome_dis, sizeof(d0[k1].nome_dis), stdin);
     printf("codigo da disciplina:\n");
-    scanf("%d", &d0[k].codigo);
+    scanf("%s", &d0[k].codigo);
     printf("semestre da disciplina:\n");
     scanf("%d", &d0[k].semestre);
     k++;  
 }
 
-void exclui_prof(int exclusao_aluno0, int i2, aluno a2[]){
+void exclui_dis(int exclusao_dis0, int k2, disciplina d2[]){
         
-        aluno aux;
+        disciplina auxd;
 
-        for(int i0 = exclusao_aluno0; i0 < i2 && i0 + 1 < i2; i0++){
-            aux = a2[i0 + 1];
-            a2[i0 + 1] = a2[i0];
-            a2[i0] = aux; 
+        for(int i0 = exclusao_dis0; i0 < k2 && i0 + 1 < k2; i0++){
+            auxd = d2[i0 + 1];
+            d2[i0 + 1] = d2[i0];
+            d2[i0] = auxd; 
+        }
+            
+    }
+
+void exclui_prof(int exclusao_prof0, int j2, professor p2[]){
+        
+        professor auxp;
+
+        for(int i0 = exclusao_prof0; i0 < j2 && i0 + 1 < j2; i0++){
+            auxp = p2[i0 + 1];
+            p2[i0 + 1] = p2[i0];
+            p2[i0] = auxp; 
         }
             
     }
 
 void exclui_aluno(int exclusao_aluno0, int i2, aluno a2[]){
         
-        aluno aux;
+        aluno auxa;
 
         for(int i0 = exclusao_aluno0; i0 < i2 && i0 + 1 < i2; i0++){
-            aux = a2[i0 + 1];
+            auxa = a2[i0 + 1];
             a2[i0 + 1] = a2[i0];
-            a2[i0] = aux; 
+            a2[i0] = auxa; 
         }
             
     }   
 
 int valida_aluno(int i4){
-    int nao_sei = 0;
+    int nao_sei1 = 0;
     if(i4 == 0){
-        nao_sei = 1; /*vazio*/
+        nao_sei1 = 1; /*vazio*/
     }
     else if(i4 == CAPACIDADE){
-        nao_sei = 2; /*cheio*/
+        nao_sei1 = 2; /*cheio*/
     }
-    return nao_sei;
+    return nao_sei1;
 
 }
 
-void lista_aluno(aluno a2[], int i3){
+int valida_prof(int j4){
+    int nao_sei2 = 0;
+    if(j4 == 0){
+        nao_sei2 = 1; /*vazio*/
+    }
+    else if(j4 == CAPACIDADE){
+        nao_sei2 = 2; /*cheio*/
+    }
+    return nao_sei2;
 
-    int cont = 0;
+}
+
+int valida_dis(int k4){
+    int nao_sei3 = 0;
+    if(k4 == 0){
+        nao_sei3 = 1; /*vazio*/
+    }
+    else if(k4 == CAPACIDADE){
+        nao_sei3 = 2; /*cheio*/
+    }
+    return nao_sei3;
+
+}
+
+void lista_aluno(aluno a3[], int i3){
+
+    int contA = 0;
     if (i3 == 0){
         printf("nenhum aluno cadastrado");
     
@@ -112,8 +148,48 @@ void lista_aluno(aluno a2[], int i3){
     else{
 
         for (int limite = 0; limite < i3; limite ++ ){
-            printf("%d- %s\n%d\n%d\n\n", cont, a2[limite].nome_aluno, a2[limite].cpf_aluno, a2[limite].matricula_aluno);
-            cont++;
+            printf("%d- %s\n%s\n%s\n\n", contA, a3[limite].nome_aluno, a3[limite].cpf_aluno, a3[limite].matricula_aluno);
+            contA++;
+        }
+
+    }
+
+
+}
+
+void lista_prof(professor p3[], int j3){
+
+    int contP = 0;
+    if (j3 == 0){
+        printf("nenhum aluno cadastrado");
+    
+    }
+    
+    else{
+
+        for (int limite = 0; limite < i3; limite ++ ){
+            printf("%d- %s\n%s\n%s\n\n", contP, p3[limite].nome_prof, p3[limite].cpf_prof, p3[limite].matricula_prof);
+            contP++;
+        }
+
+    }
+
+
+}
+
+void lista_dis(disciplina d3[], int k3){
+
+    int contD = 0;
+    if (k3 == 0){
+        printf("nenhuma disciplina cadastrada");
+    
+    }
+    
+    else{
+
+        for (int limite = 0; limite < k3; limite ++ ){
+            printf("%d- %s\nCodigo: %s\nSemestre: %d\nProfessor: %s %s %s\n\n", contD, d3[limite].nome_dis, d3[limite].codigo, d3[limite].semestre, d3[limite].prof_da_disciplina.nome_prof, d3[limite].prof_da_disciplina.matricula_prof, d3[limite].prof_da_disciplina.cpf_prof);
+            contD++;
         }
 
     }
@@ -127,12 +203,19 @@ int main(){
     professor p1[CAPACIDADE];
     aluno a1[CAPACIDADE];
     int opcao_menu;
-    int opcao_cadastro_aluno;
+    int opcao_cadastro_prof2;
     int opcao_cadastro_aluno2;
+    int opcao_cadastro_dis2;
     int opcao_cadastro;
     int atualizacao_aluno;
+    int atualizacao_prof;
+    int atualizacao_dis;
     int exclusao_aluno;
+    int exclusao_prof;
+    int exclusao_dis;
     int aluno_valido;
+    int prof_valido;
+    int dis_valido;
     int sair_menu = 0;
     int ii = 0;
     int jj = 0;
@@ -203,10 +286,60 @@ int main(){
                         break;
                     
                     case 1:
+                        printf("1- incluir professor\n2- atualizar professor\n3- excluir professor:\n");
+                        scanf("%d", &opcao_cadastro_prof2);
+                        switch(opcao_cadastro_prof2){
                         
-                        cadastra_professor(p1, jj);
-                        jj++;
+                            case 1:
+                                prof_valido = valida_prof(jj);
+                                if(prof_valido < 2){
+                                    cadastra_prof(p1, jj);
+                                    jj++;
+                                    break;
+                                }
+
+                                else{
+                                    printf("limite de professores atingido!");
+                                    break;
+                                }
+
+                            case 2:
+                                prof_valido = valida_prof(jj);
+                                if(prof_valido != 1){
+                                    printf("digite o numero correspondente ao professor que deseja atualizar:\n");
+                                    lista_prof(p1, jj);
+                                    scanf("%d", &atualizacao_prof);
+                                    cadastra_prof(a1, atualizacao_prof);
+                                    break;
+                                }
+                                
+                                else{
+                                    printf("nenhum professor cadastrado");
+                                    break;
+                                }
+
+                                break;
+                            case 3:
+                                prof_valido = valida_prof(jj);
+                                if (prof_valido != 1){
+                                    printf("digite o numero correspondente ao professor que deseja excluir:\n");
+                                    lista_prof(p1, jj);
+                                    scanf("%d", &exclusao_prof);
+                                    exclui_aluno(exclusao_aluno, ii, a1);
+                                    ii--;
+                                    break;
+                                }
+                                
+                                else{
+                                    printf("nenhum aluno cadastrado");
+                                    break;
+                                }
+
+                                }
                         break;
+                        
+                        
+                        
 
                     case 2:
 
